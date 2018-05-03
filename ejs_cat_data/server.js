b46e39a0a329841529d2bea12c9bda09611007c3
LawerenceLee <lucifitz.edward@gmail.com>
2018-05-03 10:52:22 -0700

    finished ejs_cat_data

--- ejs_cat_data/server.js
+++ ejs_cat_data/server.js
@@ -9,13 +9,31 @@ app.get("/cats", function (request, response){
   response.render('cats');
 })
 app.get("/snuggles", function (request, response){
-  response.render('kitty');
+  const cat_attrs = {
+    Name: 'Snuggles',
+    Food: 'Chicken Liver',
+    Age: '3',
+    Sleeping_Spots: ['top of the couch', 'windowsill']
+  }
+  response.render('kitty', {kitty: cat_attrs});
 })
 app.get("/charlie", function (request, response){
-  response.render('kitty');
+  const cat_attrs = {
+    Name: 'Charlie',
+    Food: 'FISH Liver',
+    Age: '5',
+    Sleeping_Spots: ['on top of the cabinet', 'in bathroom sink']
+  }
+  response.render('kitty', {kitty: cat_attrs});
 })
 app.get("/fatty", function (request, response){
-  response.render('kitty');
+  const cat_attrs = {
+    Name: 'Fatty',
+    Food: 'FISH',
+    Age: '1',
+    Sleeping_Spots: ['under of the couch', 'under the bed'],
+  }
+  response.render('kitty', {kitty: cat_attrs});
 })
 
 app.listen(portNum, () => {
